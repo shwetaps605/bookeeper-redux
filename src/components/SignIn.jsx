af396279b27e4970a4e668bf312a5b489d6826d7
shwetaps605 <shwetaroy305@gmail.com>
2022-04-04 22:02:51 +0530

    Added the SignIn component

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../features/login/loggerSlice'
+import { addUser } from '../features/users/userSlice'
+
+
+const SignIn = () => {
+  const dispatch = useDispatch()
+  const [username, setUsername] = useState("")
+
+  const handleSubmit = () => {
+    dispatch(addUser({ name: username }))
+    setUsername("")
+    dispatch(login())
+  }
+
+    return (
+        <div className="signin-container">
+            <header>
+                <h2>Sign In</h2>
+            </header>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder='enter your name'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <button onClick={() => handleSubmit()}>
+                    LogIn
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default SignIn
