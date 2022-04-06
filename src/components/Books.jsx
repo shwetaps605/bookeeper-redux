8b979ca2c712d9413ddc1257c1493d9c4fbfb04b
shwetaps605 <shwetaroy305@gmail.com>
2022-04-06 23:53:07 +0530

    added code for Books component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { addBook } from '../features/books/booksSlice'
 
 const Books = () => {
-  return (
-      <header>
-            <h2>Books</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, iste.</p>
-      </header>
+
+    const [bookTitle, setBookTitle] = useState('')
+    const [author, setAuthor] = useState('')
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addBook({ title: bookTitle, author: author })
+    }
+    return (
+        <>
+            <header>
+                <h2>Books</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi, iste.</p>
+            </header>
+            <main>
+                <form className='book-form' onSubmit={handleSubmit}>
+                    <input type="text"
+                        placeholder=' book title'
+                        value={bookTitle}
+                        onChange={(e) => setBookTitle(e.target.value)}
+                    />
+
+                    <input type="text"
+                        placeholder='author name'
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                    />
+                </form>
+            </main>
+        </>
+
     )
-    
+
 }
 
 export default Books
