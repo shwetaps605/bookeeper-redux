98be0b496333d3b36ee81d7ae9cadc52999dc1ff
shwetaps605 <shwetaroy305@gmail.com>
2022-04-06 23:47:04 +0530

    added bookSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/users/userSlice';
 import loggerReducer from '../features/login/loggerSlice'
+import booksReducer from '../features/books/booksSlice'
 
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
-    logger: loggerReducer
+    logger: loggerReducer,
+    books: booksReducer,
   },
 });
